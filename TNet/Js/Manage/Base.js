8027568548ee27515672b29d34fa3fb8dd273cd8
aixiaoxiang2009@hotmail.com <aixiaoxiang2009@hotmail.com>
2016-11-20 17:10:27 +0800

    添加滚动条

--- TNet/Js/Manage/Base.js
+++ TNet/Js/Manage/Base.js
@@ -1,7 +1,7 @@
 ﻿
 function initParam() {
     var href = window.location.href + "";
-    
+
     if ((/(Manage\/MercList)/gi).test(href) || (/(Manage\/MercEdit)/gi).test(href)) {
         $("#MercList").addClass("select");
     } else if ((/(Manage\/MercTypeList)/gi).test(href) || (/(Manage\/MercTypeEdit)/gi).test(href)) {
@@ -14,11 +14,42 @@ function initParam() {
     else if ((/(Manage\/Menu)/gi).test(href)) {
         $("#Menu").addClass("select");
     }
+    $(".CL").niceScroll({
+        //mousescrollstep: 40, // 鼠标滚轮的滚动速度 (像素)
+        //touchbehavior: true, // 激活拖拽滚动
+        hwacceleration: true, // 激活硬件加速
+        railoffset: true,
+        disableoutline: true,
+        enabletranslate3d: true, // nicescroll 可以使用CSS变型来滚动内容
+        enablemousewheel: true, // nicescroll可以管理鼠标滚轮事件
+        enablekeyboard: true, // nicescroll可以管理键盘事件
+        enablemouselockapi: true, // 可以用鼠标锁定API标题 (类似对象拖动)
+        cursorfixedheight: false, // 修正光标的高度（像素）
+        //hidecursordelay: 400, // 设置滚动条淡出的延迟时间（毫秒）
+        preventmultitouchscrolling: true,
+        boxzoom: false,
+        autohidemode: "leave"
+    });
+    updateSetResize();
+    $(window).resize(function () {
+        updateSetResize();
+    });
 }
+
+function updateSetResize() {
+    $(".CL").getNiceScroll().resize();
+    var mh = $(".CR").outerHeight(true) + 20;
+    var dh = $(document).height() - 50;
+    if (mh > dh) {
+        mh = dh;
+    }
+    $(".CR").css("height", mh);
+}
+
 
 $(document.body).ready(initParam);
 
-function ajax_del(delUrl,postData) {
+function ajax_del(delUrl, postData) {
     $.post(delUrl, postData, function (returndata) {
         returndata = eval("(" + returndata + ")");
         if (returndata.Code == "1") {
