c2ab294cecdda093a6b8d431e5ddbd200769f784
yangbinqi <ybq@outlook.com>
2016-11-23 22:54:56 +0800

    fix datetimepicker

--- TNet/Js/Manage/Base.js
+++ TNet/Js/Manage/Base.js
@@ -102,3 +102,18 @@ function enableRecord(postUrl) {
         }
     });
 }
+
+function bindDatetimePicker(){
+    $('.form_datetime').datetimepicker({
+        language: 'zh-CN',
+        weekStart: 1,
+        todayBtn: 1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        minView: 2,
+        forceParse: 0,
+        format: 'yyyy-mm-dd'
+    });
+}
+
